fef9d026759fee18a98c7e4cb55b3ff30bf40453
sirius4galaxy <uniswap_v2@163.com>
2025-03-18 18:07:38 +0800

    fix serialization error of transaction trace in state_history_plugin

--- libraries/state_history/include/eosio/state_history/serialization.hpp
+++ libraries/state_history/include/eosio/state_history/serialization.hpp
@@ -670,7 +670,7 @@ datastream<ST>& operator<<(datastream<ST>& ds, const history_context_wrapper_sta
       fc::raw::pack(ds, fc::unsigned_int(0));
    }
    fc::raw::pack(ds, as_type<int64_t>(debug_mode ? trace.elapsed.count() : 0));
-   fc::raw::pack(ds, as_type<eosio::chain::transaction_res_usage>(trace.res_usage));
+   fc::raw::pack( ds, make_history_serial_wrapper(as_type<eosio::chain::transaction_res_usage>(trace.res_usage)) );
    fc::raw::pack(ds, as_type<bool>(trace.scheduled));
    history_context_serialize_container(ds, debug_mode, as_type<std::vector<eosio::chain::action_trace>>(trace.action_traces));
 
