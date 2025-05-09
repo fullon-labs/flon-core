f7232f60cad752f6ec226abbacf266d6c2ec799a
sirius4galaxy <uniswap_v2@163.com>
2025-05-09 10:57:08 +0800

    update data of unittest

--- unittests/savanna_misc_tests.cpp
+++ unittests/savanna_misc_tests.cpp
@@ -802,6 +802,7 @@ BOOST_FIXTURE_TEST_CASE(verify_block_compatibitity, savanna_cluster::cluster_t) 
    auto b9_id = b9->calculate_id();
 
    if (save_blockchain) {
+      ilog("save blockchain data and end.");
       save_blockchain_data(ref_blockchain_path, A.get_config().blocks_dir, b9_id, snapshot);
       return;
    }
